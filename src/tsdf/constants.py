b269cd6d13027c26c6fd8ef6d97bddb979c6746e
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2022-12-19 14:10:33 +0100

    Add MANDATORY_KEYS to tsdf.constants

--- src/tsdf/constants.py
+++ src/tsdf/constants.py
@@ -1,1 +1,4 @@
 SUPPORTED_VERSIONS = ["0.1"]
+
+# Dictionary linking mandatory keys for different versions
+MANDATORY_KEYS = { "0.1" : ["subject_id","study_id","device_id", "endianness","metadata_version","data_type", "bits", "rows","channels", "units","file_name","start_iso8601","end_iso8601"] }
--- src/tsdf/io.py
+++ src/tsdf/io.py
@@ -31,14 +31,7 @@ def _check(data):
     assert version in SUPPORTED_VERSIONS, f"TSDF file version {version} not supported."
 
     # Check that mandatory keys are present
-    # TODO: move to constants.py
-    mandatory_keys = ["subject_id","study_id","device_id",
-                      "endianness","metadata_version","data_type",
-                      "bits", "rows","channels",
-                      "units","file_name","start_iso8601","end_iso8601"]
-
     # TODO: the snippet below only tests keys at depth 1. As a consequence, it 
     # works fine for flat.json, but fails with hierarchical.json
-    #
-    # for key in mandatory_keys:
+    # for key in MANDATORY_KEYS[version]:
     #    data[key] # Throws error if a key is not present
