bd81d1d4041c83af620b5f8e7317e9d273bffcf7
Peter Kok <kretep@users.noreply.github.com>
2023-01-09 11:53:35 +0100

    Fix io_metadata tests

--- tests/test_io_metadata.py
+++ tests/test_io_metadata.py
@@ -14,7 +14,7 @@ TESTDATA = { "flat"         : os.path.join(TESTDATA_DIR, 'flat.json'),
 class TestWrongFormatting(unittest.TestCase):
     """ Test whether the exceptions are thrown in case the metadata file is not well annotated. """
 
-    def load_wrong_version(self):
+    def test_load_wrong_version(self):
         """ Test that a file with a wrong version raises an exception. """
         with open(TESTDATA["wrongversion"], 'r') as file:
             with self.assertRaises(AssertionError) as context:
@@ -24,7 +24,7 @@ class TestWrongFormatting(unittest.TestCase):
             self.assertTrue("TSDF file version" in str(context.exception), 
             "Wrong version is not being detected")
 
-    def load_missing_key(self):
+    def test_load_missing_key(self):
         """ Test that a file with a missing mandatory key raises an exception. """
         with open(TESTDATA["missingkey"], 'r') as file:
             with self.assertRaises(AssertionError) as context:
@@ -37,7 +37,7 @@ class TestWrongFormatting(unittest.TestCase):
 class TestTSDFMetadataParsing(unittest.TestCase):
     """ Test whether the TSDF objects are well specified are well defined. """
 
-    def load_flat_structure(self):
+    def test_load_flat_structure(self):
         """ Test parsing of a flat TSDF metadata file. """
         with open(TESTDATA["flat"], 'r') as file:
             data = json.load(file)
