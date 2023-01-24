be6bbe84426d821ebebb89f0c780c63a842f2d85
Peter Kok <kretep@users.noreply.github.com>
2023-01-24 17:28:54 +0100

    Explicitly expose functions in tsdf package

--- src/tsdf/__init__.py
+++ src/tsdf/__init__.py
@@ -1,5 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tsdf")
-import tsdf.io
-import tsdf.io_metadata
+
+from .io import load_file, load_from_path, load_string, \
+  load_binary_from_metadata, load_binary_file, get_metadata_from_ndarray, save_binary_file
+from .tsdf_metadata import TSDFMetadata
